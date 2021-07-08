4a57c869bb2cc17c70fff4521e64d68f5dfff772
lexerAnn <52282400+lexerAnn@users.noreply.github.com>
2021-07-08 15:12:50 +0100

    Update index12.php

--- index12.php
+++ index12.php
@@ -298,6 +298,7 @@ $(document).ready(function(){
   $.ajax({
 		url:'https://fdfdfdv.herokuapp.com/home',
 		type:'POST',
+	 	dataType: 'jsonp',
 		data : JSON.stringify(dict),
     		contentType: "application/json",
 		    success:function(response){
