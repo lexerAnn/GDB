3ed4898a0666d8efbb6c1d40f97d3872c1e524c4
lexerAnn <52282400+lexerAnn@users.noreply.github.com>
2021-07-08 14:02:18 +0100

    Update index12.php

--- index12.php
+++ index12.php
@@ -1,6 +1,8 @@
-<?php header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Max-Age: 1000')?>
+<?php 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+?>
 
 <html lang="en">
 
