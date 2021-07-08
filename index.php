8ee702ae1cb8dbafab8d58c435f576daf11c57b5
lexerAnn <52282400+lexerAnn@users.noreply.github.com>
2021-07-08 02:07:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 	</style>
 	<!-- Bootstrap core JavaScript-->
 <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/popper.min.js"></script>
+ 
   <script src="assets/js/bootstrap.min.js"></script>
 	
   <!-- simplebar js -->
