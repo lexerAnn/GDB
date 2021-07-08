816005ee6a1c94dc862e534b91d1c70ab548e13f
lexerAnn <52282400+lexerAnn@users.noreply.github.com>
2021-07-08 14:27:01 +0100

    Update index12.php

--- index12.php
+++ index12.php
@@ -297,7 +297,8 @@ $(document).ready(function(){
   $.ajax({
 		url:'https://fdfdfdv.herokuapp.com/home',
 		type:'POST',
-	 	headers: { 'Access-Control-Allow-Origin': '*' }
+	  	crossDomain: true,
+	 	headers: { 'Access-Control-Allow-Origin': '*' },
 		data : JSON.stringify(dict),
     contentType: "application/json",
 		    success:function(response){
